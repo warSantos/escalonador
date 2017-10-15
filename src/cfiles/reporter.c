1e586f38f86636e8e638fd47d96560a3aa83f730
ygorrc <32312761+ygorrc@users.noreply.github.com>
2017-10-15 02:55:46 -0200

    Add files via upload

--- src/cfiles/reporter.c
+++ src/cfiles/reporter.c
@@ -1,9 +1,35 @@
 #include "../headers/reporter.h"
 
 
-void showTable(Processo *p, sint estado, sint legenda0, sint legenda1){
-/*
- Implemente aqui a função para 
- */
+void showTable(Processo *p, sint legenda0, sint legenda1,sint aux ,sint aux1){
+if(legenda0 == 1){
+	printf("**************************************************************************************\n");
+	printf("Estado do Sistema:\n");
+	printf("**************************************************************************************\n");
+	printf("TEMPO ATUAL:%d\n",/*variavel que ta faltando*/ );
+}
+else if(legenda0 == 0){
+	if(legenda1 == 1){
+		printf("PROCESSO EXECUTANDO:\n");
+		printf("PID\tPPID\tPRIORIDADE\tVALOR\tTEMPO INI\tCPU USADA\n");
+		printf("%d\t%d\t%d\t%d\t%d\t%d\n",p->pid,p->pidPai,p->prioridade,p->valorInteiro,p->tempoInicio,p->tempoAcumulado);
+	}
+	if(legenda1 == 2){
+		if(aux = 1){
+			printf("PROCESSO BLOQUEADO:");
+			printf("PID\tPPID\tPRIORIDADE\tVALOR\tTEMPO INI\tCPU USADA\n");
+			aux = 0;
+		}	
+		printf("%d\t%d\t%d\t%d\t%d\t%d\n",p->pid,p->pidPai,p->prioridade,p->valorInteiro,p->tempoInicio,p->tempoAcumulado);
+	}
+	if(legenda1 == 3){
+		if(aux1 ==1){
+			printf("PROCESSO PRONTOS:");
+			printf("PID\tPPID\tPRIORIDADE\tVALOR\tTEMPO INI\tCPU USADA\n");
+			aux1 = 0;
+		}
+		printf("%d\t%d\t%d\t%d\t%d\t%d\n",p->pid,p->pidPai,p->prioridade,p->valorInteiro,p->tempoInicio,p->tempoAcumulado);
+	}
+}
 
 }
